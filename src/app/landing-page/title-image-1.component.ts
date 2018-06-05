c270c162facc6e76912eda274f7abd2639d4578d
Florian Winkler <florian_winkler@kw-studios.com>
2018-06-05 17:26:21 +0200

    Add jpg  fallbacks for browser that dont support webp

--- src/app/landing-page/title-image-1.component.ts
+++ src/app/landing-page/title-image-1.component.ts
@@ -8,12 +8,22 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
         [lazyLoad]="images"
         [offset]="offset"
         [useSrcset]="true">
+        <picture>
+            <source [attr.defaultImage]="defaultImage" [attr.lazyLoad]="imageswebp" type="image/webp">
+            <source [attr.defaultImage]="defaultImage" [attr.lazyLoad]="imagesjpeg" type="image/jpeg">
+            <img [defaultImage]="defaultImage"
+                 [lazyLoad]="imagejpg"
+                 [offset]="offset">
+        </picture>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleImage1Component {
     defaultImage = 'assets/images/title2lowres.webp';
-    images = `assets/images/title21x.webp 1076w,
+    imagejpg = 'assets/images/title2.jpeg';
+    imageswebp = `assets/images/title21x.webp 1076w,
      assets/images/title2.webp 1980w`;
+    imagesjpeg = `assets/images/title21x.jpeg 1076w,
+      assets/images/title2.jpeg 1980w`;
     offset = 100;
 }
--- src/app/landing-page/title-image-2.component.ts
+++ src/app/landing-page/title-image-2.component.ts
@@ -3,17 +3,22 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
     selector: 'app-title-image-2',
     template: `
-        <img
-        [defaultImage]="defaultImage"
-        [lazyLoad]="images"
-        [offset]="offset"
-        [useSrcset]="true">
+        <picture>
+            <source [attr.defaultImage]="defaultImage" [attr.lazyLoad]="imageswebp" type="image/webp">
+            <source [attr.defaultImage]="defaultImage" [attr.lazyLoad]="imagesjpg" type="image/jpeg">
+            <img [defaultImage]="defaultImage"
+                 [lazyLoad]="imagejpg"
+                 [offset]="offset">
+        </picture>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleImage2Component {
     defaultImage = 'assets/images/title3lowres.jpg';
-    images = `assets/images/title31x.jpg 972w,
+    imagejpg = 'assets/images/title3.jpg'
+    imageswebp = `assets/images/title31x.jpg 972w,
      assets/images/title3.webp 1980w`;
+    imagesjpg = `assets/images/title31x.jpg 972w,
+     assets/images/title3.jpg 1980w`;
     offset = 100;
 }
